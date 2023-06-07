8fd0274be3e4c3f8cdde0acca94e8b0ca0356a99
Nathan Englert <nenglert@gmail.com>
2023-06-06 19:12:45 -0500

    Enable backspace to remove selected items in combobox

--- components/ui/combobox.tsx
+++ components/ui/combobox.tsx
@@ -30,7 +30,22 @@ export function Combobox({
   items,
   onChange,
 }: ComboBoxProps) {
-  const [selectedItems, setSelectedItems] = useState([])
+  const [selectedItems, setSelectedItems] = useState<ComboboxItem[]>([])
+  const [query, setQuery] = useState("")
+
+  const lowercasedQuery = query.toLowerCase()
+  const isNotSelected = (item: ComboboxItem) => !selectedItems.includes(item)
+  const matchesQuery = (item: ComboboxItem) =>
+    item.label.toLowerCase().includes(lowercasedQuery)
+
+  const filteredItems = items.filter(
+    (item) => isNotSelected(item) && (query === "" || matchesQuery(item))
+  )
+
+  const handleQueryKeyDown = (evt: React.KeyboardEvent<HTMLInputElement>) => {
+    if (query !== "" || evt.key !== "Backspace") return
+    setSelectedItems(selectedItems.slice(0, -1))
+  }
 
   const removeItem = (itemToRemove: ComboboxItem) => {
     setSelectedItems(selectedItems.filter((item) => item !== itemToRemove))
@@ -39,19 +54,23 @@ export function Combobox({
   return (
     <HeadlessCombobox
       value={selectedItems}
-      onChange={(value: []) => setSelectedItems(value)}
+      onChange={(value: []) => {
+        setSelectedItems(value)
+        setQuery("")
+      }}
       multiple
     >
       <Float as="div" className="relative" floatingAs={React.Fragment}>
         <div
           className={cn(
-            "flex h-10 gap-2 w-full rounded-md border border-input bg-transparent px-3 py-2 text-sm ring-offset-background placeholder:text-muted-foreground focus-within:ring-2 focus-within:ring-ring focus-within:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50",
+            "flex h-10 gap-2 w-full rounded-md border border-input bg-transparent px-3 py-2 text-sm ring-offset-background placeholder:text-muted-foreground focus-within:ring-2 focus-within:ring-ring focus-within:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 overflow-hidden",
             className
           )}
         >
           {selectedItems.map((item: ComboboxItem) => (
             <Button
               type="button"
+              key={item.label}
               size={`xs`}
               variant={`secondary`}
               className="gap-2"
@@ -63,6 +82,9 @@ export function Combobox({
           <HeadlessCombobox.Input
             className={`flex-grow focus-visible:outline-none`}
             placeholder={placeholder}
+            value={query}
+            onKeyDown={handleQueryKeyDown}
+            onChange={(evt) => setQuery(evt.target.value)}
           />
         </div>
         <HeadlessCombobox.Options
@@ -70,30 +92,19 @@ export function Combobox({
             "z-50 w-full mt-1 rounded-md border bg-popover p-0 text-popover-foreground shadow-md outline-none animate-in data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2"
           )}
         >
-          {items.map((item) => (
+          {filteredItems.map((item) => (
             <HeadlessCombobox.Option
               key={item.value}
               value={item}
               as={React.Fragment}
             >
-              {({
-                active,
-                selected,
-              }: {
-                active: boolean
-                selected: boolean
-              }) => (
+              {({ active }: { active: boolean }) => (
                 <li
                   className={cn(
                     "relative flex w-full cursor-default items-center rounded-sm py-1.5 pl-8 pr-2 text-sm outline-none data-[disabled]:pointer-events-none data-[disabled]:opacity-50",
                     active ? "bg-accent text-accent-foreground" : ""
                   )}
                 >
-                  {selected && (
-                    <span className="absolute left-2 flex h-3.5 w-3.5 items-center justify-center">
-                      <Check className="h-4 w-4" />
-                    </span>
-                  )}
                   {item.label}
                 </li>
               )}
